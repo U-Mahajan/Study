42e878017688264668e7c9cf8669393131344ce8
u-mahajan <umesh.mahajan@in.ncs-i.com>
2023-10-31 14:44:34 +0530

    "new change"

--- src/main/java/com/rms/student/controller/StudentController.java
+++ src/main/java/com/rms/student/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     @GetMapping("/add")
     public String sdd() {
+        System.out.println("ffff");
         return "umesh";
     }
 }
