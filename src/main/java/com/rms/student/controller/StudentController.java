3340a14651dcd14b07493c4483cd47fbebd19de3
u-mahajan <umesh.mahajan@in.ncs-i.com>
2023-10-31 14:40:46 +0530

    "fffff"

--- src/main/java/com/rms/student/controller/StudentController.java
+++ src/main/java/com/rms/student/controller/StudentController.java
@@ -1,4 +1,14 @@
 package com.rms.student.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class StudentController {
+    @GetMapping("/add")
+    public String sdd() {
+        return "umesh";
+    }
 }
